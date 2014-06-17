7650cae271d56d396ed7ad18aeeef14688979e9a
t00n <kelkununtel@hotmail.com>
2014-06-17 13:35:40 +0200

    [add] So lame query system : parsing and exec.... x)

--- Tagger.py
+++ Tagger.py
@@ -2,6 +2,8 @@ import os
 import json
 import gtk
 import hashlib
+import argparse
+import re
 from Image import Image
 from ConfigParser import ConfigParser
 
@@ -33,19 +35,21 @@ class Tagger:
 		except:
 			print("scanning...")
 			self.scan()
+		self.save()
 
 	def save(self):
 		with open(self.TAGFILE, "w") as f:
 			json.dump(dict((imagehash, img.__dict__) for imagehash, img in self.images.iteritems()), f)
 			f.close()
 
-	def scan(self):
-		for afile in os.listdir("."):
-			if (Image.isImage(afile)):
-				self.addImage(afile)
-			elif (os.path.isdir(afile)):
-				self.scan(afile)
-		self.save()
+	def scan(self, directory = "."):
+		for afile in os.listdir(directory):
+			filename = directory + "/" + afile
+			print(filename)
+			if (Image.isImage(filename)):
+				self.addImage(filename)
+			elif (os.path.isdir(filename)):
+				self.scan(filename)
 
 	def addImage(self, afile):
 		with open(afile) as f:
@@ -53,6 +57,26 @@ class Tagger:
 			if (hach not in self.images):
 				self.images[hach] = Image(afile)
 			f.close()
+
+	def query(self, query, subset = None):
+		query = "\"" + query
+		index = [(m.start(0), m.end(0)) for m in re.finditer(" and | or ", query)]
+		previous = 0
+		for (start, end) in index:
+			j = start + previous
+			k = end + previous
+			if (query[k:k+3] == "not"):
+				k += 4
+			query = query[:k] + "\"" + query[k:]
+			query = query[:j] + "\" in image.tags" + query[j:]
+			previous += len("\" in image.tagss")
+		query = query + "\" in image.tags"
+		print(query)
+
+		ret = []
+		for imagehash, image in self.images.iteritems():
+			exec("if (" + query + "): ret.append(image)")
+		print(ret)
 
 class MainWindow(gtk.Window):
 	def __init__(self, tagger):
@@ -62,9 +86,13 @@ class MainWindow(gtk.Window):
 		# gtk.main()
 
 if __name__ == '__main__':
-	folder = "test/"
-	tagger = Tagger(folder)
-	print(tagger.images)
-	# tagger.scan()
+	parser = argparse.ArgumentParser(description="Tag your pictures and view it with your favorite image viewer !")
+	parser.add_argument("directory", type=str, help="The pictures directory")
+	args = parser.parse_args()
+	tagger = Tagger(args.directory)
 	# print(tagger.images)
-	window = MainWindow(tagger)
+	# window = MainWindow(tagger)
+	# queryAnd = tagger.query("01 and Dragon Ball or not Dragon Ball or not 03")
+	queryAnd = tagger.query("a\"): print(\"caca\"); a=\"\" \nif(\"")
+	# 01 in ... and Dragon Ball in ... or not 05 in ... or 03 in ... and Dragon Ball in ...
+	print(queryAnd)
