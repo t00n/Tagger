76e0c99d56df096cdfa09fb2ddbb45c43bd119e0
t00n <antoine.carpentier.info@gmail.com>
2015-04-30 13:18:51 +0200

    [add] escape to focus on images, left and right to change images

--- TagGui.py
+++ TagGui.py
@@ -6,39 +6,53 @@ from Collection import Collection
 import MainWindowUI
 
 class TagGuiWindow(QtGui.QMainWindow, MainWindowUI.Ui_MainWindow):
+    WINDOW_TITLE = "TagGui"
     def __init__(self, config, parent=None):
         super(TagGuiWindow, self).__init__(parent)
         self.setupUi(self)
-        self.actionOpen.triggered.connect(self.openCollection)
-        self.actionScan.triggered.connect(self.scanCollection)
-        self.actionSave.triggered.connect(self.saveCollection)
+        self.actionOpen.triggered.connect(self._openCollection)
+        self.actionScan.triggered.connect(self._scanCollection)
+        self.actionSave.triggered.connect(self._saveCollection)
         self.actionExit.triggered.connect(self.close)
 
-        self.actionAdd_tags.triggered.connect(self.addTags)
-        self.actionRemove_tags.triggered.connect(self.removeTags)
+        self.actionAdd_tags.triggered.connect(self._addTags)
+        self.actionRemove_tags.triggered.connect(self._removeTags)
 
         
     def keyPressEvent(self, event):
         """ """
-        if event.key() == QtCore.Qt.Key_Left or event.key() == QtCore.Qt.Key_Escape:
-            self.prevImage()
+        if event.key() == QtCore.Qt.Key_Left:
+            self._prevImage()
         elif event.key() == QtCore.Qt.Key_Right:
-            self.nextImage()
+            self._nextImage()
+        elif event.key() == QtCore.Qt.Key_Escape:
+            self.setFocus()
+        elif event.key() == QtCore.Qt.Key_Enter:
+            # TODO query
+            pass
 
-    def prevImage(self):
+    def mousePressEvent(self, event):
+        # TODO zoom in/out
+        pass
+
+    # TODO select current image (for tags)
+    def _selectImage(self, index):
+        self.stackedWidget.setCurrentIndex(index)
+        self.windowTitle = self.WINDOW_TITLE + " - "
+
+    def _prevImage(self):
         index = self.stackedWidget.currentIndex() - 1
         if index < 0:
             index = self.stackedWidget.count() - 1
-        self.stackedWidget.setCurrentIndex(index)
+        self._selectImage(index)
 
-    def nextImage(self):
+    def _nextImage(self):
         index = self.stackedWidget.currentIndex() + 1
         if index >= self.stackedWidget.count():
             index = 0
-        self.stackedWidget.setCurrentIndex(index)
+        self._selectImage(index)
 
-
-    def openCollection(self):
+    def _openCollection(self):
         dir = QtGui.QFileDialog.getExistingDirectory()
         if dir != "":
             self.collection = Collection(dir)
@@ -49,18 +63,20 @@ class TagGuiWindow(QtGui.QMainWindow, MainWindowUI.Ui_MainWindow):
                 # label.setText(image.location)
                 self.stackedWidget.addWidget(label)
 
-    def scanCollection(self):
+    def _scanCollection(self):
         if self.collection:
             self.collection.scan()
 
-    def saveCollection(self):
+    def _saveCollection(self):
         if self.collection:
             self.collection.save()
 
-    def addTags(self):
+    def _addTags(self):
+        # TODO add tags using a dialog
         pass
 
-    def removeTags(self):
+    def _removeTags(self):
+        # TODO remove tags using a dialog
         pass
 
 if __name__ == '__main__':
